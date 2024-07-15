bb735e4ea0294b4746089617cce7aa3c06260808
weslleyAlaniz <weslley.alaniz40@gmail.com>
2024-07-15 18:38:19 -0300

    Aumentando para 100 o numero secreto

--- Jogo do Número Secreto/app.js
+++ Jogo do Número Secreto/app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio() ;
 let tentativas = 1;
 exibirMensagemincial()
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto){
  
 function exibirMensagemincial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um númemo de 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um númemo de 1 e 100');
 }
 
 
